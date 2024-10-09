bb32aa6e701a7c9b4b6c1d54c9bffac4017dc657
Harshal Patil <harsshal@gmail.com>
2024-10-09 12:32:16 -0400

    some more recatoring of ibapi

--- ibapi/ibkr_data.py
+++ ibapi/ibkr_data.py
@@ -2,64 +2,71 @@
 
 import pandas as pd
 from ibkr_base import IBBase
+from time import sleep
 
 
 class IBHistoricalData(IBBase):
-    def __init__(self, ticker, endDateTime, durationStr, barSizeSetting):
+    def __init__(self, ticker, endDateTime, durationStr, barSizeSetting, primaryExchange="SMART"):
         super().__init__()
         self.ticker = ticker
         self.endDateTime = endDateTime
         self.durationStr = durationStr
         self.barSizeSetting = barSizeSetting
+        self.primaryExchange = primaryExchange
         self.data = []
 
-    def nextValidId(self, orderId):
-        self.request_historical_data()
-
-    def request_historical_data(self):
-        contract = self.create_contract(self.ticker)
+    def start(self):
+        contract = self.create_contract(self.ticker, self.primaryExchange)
         #self.reqHistoricalData(1, contract, self.endDateTime, self.durationStr,
         #                       self.bar_size, 'TRADES', 1, 1, False, [])
         
+        keepUpToDate = False
+        
+        if self.endDateTime == '':
+            keepUpToDate = True
+
         self.reqHistoricalData(reqId=101, 
-                          contract=contract,
-                          endDateTime=self.endDateTime, 
-                          durationStr=self.durationStr,
-                          barSizeSetting=self.barSizeSetting,
-                          whatToShow='Trades',
-                          useRTH=0,                 #0 = Includes data outside of RTH | 1 = RTH data only 
-                          formatDate=1,    
-                          keepUpToDate=0,           #0 = False | 1 = True 
-                          chartOptions=[])
+                        contract=contract,
+                        endDateTime=self.endDateTime, 
+                        durationStr=self.durationStr,
+                        barSizeSetting=self.barSizeSetting,
+                        whatToShow='Trades',
+                        useRTH=0,                 #0 = Includes data outside of RTH | 1 = RTH data only 
+                        formatDate=1,    
+                        keepUpToDate=keepUpToDate,           #0 = False | 1 = True 
+                        chartOptions=[])
+
+        sleep(1)
+        self.disconnect_client()
 
     def historicalData(self, reqId, bar):
         self.data.append(bar)
 
     def historicalDataEnd(self, reqId, start, end):
         self.disconnect_client()
-        self.process_historical_data()
-
-    def process_historical_data(self):
         attributes = vars(self.data[0]).keys() if self.data else []
         df = pd.DataFrame([{attr.title(): getattr(bar, attr) for attr in attributes}
                            for bar in self.data])
         df['Date'] = pd.to_datetime(df['Date']).dt.tz_localize('America/New_York')
-        self.df = df.set_index('Date') if not df.empty else pd.DataFrame()
-
-    def get_df(self):
-        return getattr(self, 'df', pd.DataFrame())
+        self.data = df.set_index('Date') if not df.empty else pd.DataFrame()
 
 
-def get_ibkr_data(ticker, endDateTime='', 
-                          durationStr='1 D',
-                          barSizeSetting='1 hour',):
-    app = IBHistoricalData(ticker, endDateTime, durationStr, barSizeSetting)
+def get_ibkr_data(ticker,endDateTime='', 
+                        durationStr='1 D',
+                        barSizeSetting='1 hour',
+                        primaryExchange="SMART"):
+    app = IBHistoricalData(ticker, endDateTime, durationStr, barSizeSetting, primaryExchange)
     app.run_client()
-    return app.get_df()
+    if len(app.data) == 0:
+        print("No data received for ticker: " + ticker)
+        exit()
+    return app.data
 
 
 def main():
     df = get_ibkr_data('AAPL', '20240931 00:00:00 US/Eastern', '1 D', '1 min')
+    print(df.tail(10))
+    df = get_ibkr_data('AAPL', '', '1 D', '1 min')
     print(df.tail(10))
 
 
--- strats/weekly_returns.py
+++ strats/weekly_returns.py
@@ -3,7 +3,9 @@ import pandas as pd
 import numpy as np
 
 # Step 1: Download S&P 500 historical data (last 20 years)
-sp500 = yf.download('^GSPC', start='2003-09-23', end='2023-09-23')
+sp500 = yf.download('^GSPC', start='1999-09-23', end='2023-09-23')
+sp500 = yf.download('^IXIC', start='1999-09-23', end='2023-09-23')
+sp500 = yf.download('^NSEI', start='1999-09-23', end='2023-09-23')
 
 # Step 2: Calculate daily returns
 sp500['Return'] = sp500['Adj Close'].pct_change() + 1  # daily return factor, e.g., 1.01 for 1% return
@@ -11,8 +13,6 @@ sp500['Return'] = sp500['Adj Close'].pct_change() + 1  # daily return factor, e.
 sp500['Day of Week'] = sp500.index.dayofweek  # 0 for Monday, 1 for Tuesday, ..., 6 for Sunday
 
 sp500 = sp500.dropna()[3:]
-
-sp500
 
 sip_returns = {0:100, 1:100, 2:100, 3:100, 4:100}
 for index,row in sp500.iterrows():
